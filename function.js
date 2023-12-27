d175d74ff356d144fc45fdb0f1ec0658ad229764
Jack Ooi <jack.ooi@bp.com>
2023-12-27 11:40:24 +0800

    modified:   function.js

--- function.js
+++ function.js
@@ -1,27 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const _fetchData = exports.printFormat = exports.format = exports.addStrings = void 0;
-export { _fetchData as fetchData };
+exports.fetchData = exports.printFormat = exports.format = exports.addStrings = void 0;
 function addNumbers(a, b) {
     return a + b;
 }
-const _default = addNumbers;
-export { _default as default };
+exports.default = addNumbers;
 var addStrings = function (str1, str2) { return "".concat(str1, " ").concat(str2); };
-const _addStrings = addStrings;
-export { _addStrings as addStrings };
+exports.addStrings = addStrings;
 var format = function (title, param) { return "".concat(title, " ").concat(param); };
-const _format = format;
-export { _format as format };
+exports.format = format;
 var printFormat = function (title, param) {
-    console.log((0, _format)(title, param));
+    console.log((0, exports.format)(title, param));
 };
 
-const _printFormat = printFormat;
-export { _printFormat as printFormat };
+exports.printFormat = printFormat;
 var fetchData = function (url) { return Promise.resolve("Data from ".concat(url)); };
-const _fetchData = fetchData;
-export { _fetchData as fetchData };
+exports.fetchData = fetchData;
 function introduce(salutation) {
     var names = [];
     for (var _i = 1; _i < arguments.length; _i++) {
