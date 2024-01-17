7966ecc24b8feac187a65db169572fdfa85cc429
jack.ooi <jack.ooi@bp.com>
2024-01-17 09:10:25 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   basic.ts
    	modified:   funcs-and-funcs.ts
    	modified:   function-test.ts
    	modified:   function.ts
    	modified:   tsconfig.json
    	modified:   tuples.ts

--- function-test.ts
+++ function-test.ts
@@ -2,4 +2,4 @@ import addNumbers, { addStrings, getName } from "./function";
 
 console.log(addNumbers(1, 2));
 console.log(addStrings("a", "b"));
-console.log(getName({ first: "Jack", last: 'Slayer' }));
+console.log(getName({ first: "Jack", last: "Slayer" }));
--- function.ts
+++ function.ts
@@ -1,22 +1,25 @@
 function addNumbers(a: number, b: number): number {
-    return a + b;
+  return a + b;
 }
 
 export default addNumbers;
 
-export const addStrings = (str1: string, str2: string): string => `${str1} ${str2}`;
+export const addStrings = (str1: string, str2: string): string =>
+  `${str1} ${str2}`;
 
-export const format = (title: string, param: string | number): string => `${title} ${param}`;
+export const format = (title: string, param: string | number): string =>
+  `${title} ${param}`;
 
 export const printFormat = (title: string, param: string | number): void => {
-    console.log(format(title, param));
+  console.log(format(title, param));
 };
 
-export const fetchData = (url: string): Promise<string> => Promise.resolve(`Data from ${url}`);
+export const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Data from ${url}`);
 
 function introduce(salutation: string, ...names: string[]): string {
-    return `${salutation} ${names.join(" ")}`;
+  return `${salutation} ${names.join(" ")}`;
 }
-export function getName(user: { first: string, last: string }) {
-    return `${user?.first ?? "first"} ${user?.last ?? "last"}`;
+export function getName(user: { first: string; last: string }) {
+  return `${user?.first ?? "first"} ${user?.last ?? "last"}`;
 }
