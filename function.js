8e44fe88c9568b1ccd4c3f02688fd87f171b120a
jack.ooi <jack.ooi@bp.com>
2024-01-17 10:23:09 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   funcs-and-funcs.ts
    	modified:   function.js
    	modified:   generics.ts
    	modified:   package-lock.json
    	modified:   package.json

--- function.js
+++ function.js
@@ -1,35 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchData =
-  exports.printFormat =
-  exports.format =
-  exports.addStrings =
-    void 0;
+	exports.printFormat =
+	exports.format =
+	exports.addStrings =
+		void 0;
 function addNumbers(a, b) {
-  return a + b;
+	return a + b;
 }
 exports.default = addNumbers;
 var addStrings = function (str1, str2) {
-  return "".concat(str1, " ").concat(str2);
+	return "".concat(str1, " ").concat(str2);
 };
 exports.addStrings = addStrings;
 var format = function (title, param) {
-  return "".concat(title, " ").concat(param);
+	return "".concat(title, " ").concat(param);
 };
 exports.format = format;
 var printFormat = function (title, param) {
-  console.log((0, exports.format)(title, param));
+	console.log((0, exports.format)(title, param));
 };
 
 exports.printFormat = printFormat;
 var fetchData = function (url) {
-  return Promise.resolve("Data from ".concat(url));
+	return Promise.resolve("Data from ".concat(url));
 };
 exports.fetchData = fetchData;
 function introduce(salutation) {
-  var names = [];
-  for (var _i = 1; _i < arguments.length; _i++) {
-    names[_i - 1] = arguments[_i];
-  }
-  return "".concat(salutation, " ").concat(names.join(" "));
+	var names = [];
+	for (var _i = 1; _i < arguments.length; _i++) {
+		names[_i - 1] = arguments[_i];
+	}
+	return "".concat(salutation, " ").concat(names.join(" "));
 }
