dd3564cac57f8d8a85773e240246dc10893d3225
jack.ooi <jack.ooi@bp.com>
2024-01-19 13:45:30 +0800

    modified:   utility-types-1.ts

--- utility-types-1.ts
+++ utility-types-1.ts
@@ -2,13 +2,16 @@ interface MyUser {
   name: string;
   id: string;
   email?: string;
+  phone?: string;
 }
 
-interface MyUserOptionals {
-  name?: string;
-  id?: string;
-  email?: string;
-}
+type MyUserOptionals = Partial<MyUser>;
+
+// interface MyUserOptionals {
+//   name?: string;
+//   id?: string;
+//   email?: string;
+// }
 
 const merge = (user: MyUser, overrides: MyUserOptionals): MyUser => {
   return {
@@ -19,4 +22,21 @@ const merge = (user: MyUser, overrides: MyUserOptionals): MyUser => {
 
 console.log(
   merge({ name: "Jack", id: "1", email: "jack@example.com" }, { id: "2" })
+);
+
+type RequiredMyUser = Required<MyUser>;
+type JustEmailAndName = Pick<MyUser, "email" | "name">;
+
+const mapById = (users: MyUser[]): Record<string, Omit<MyUser, "id">> => {
+  return users.reduce((a, v) => {
+    const { id, ...other } = v;
+    return { ...a, [id]: other };
+  }, {});
+};
+
+console.log(
+  mapById([
+    { id: "foo", name: "Mr. Foo" },
+    { id: "baz", name: "Mrs. Baz" },
+  ])
 );
