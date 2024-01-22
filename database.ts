1c6e8f4ca9cf8cbe2fc9d7ca4d5ec632f564d2b7
jack.ooi <jack.ooi@bp.com>
2024-01-22 10:09:37 +0800

    add 2 more exersise file

---
+++ database.ts
@@ -0,0 +1,36 @@
+interface Database {
+  get(id: string): string;
+  set(id: string, value: string): void;
+}
+
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storedState: string): void;
+}
+
+class InMemoryDatabase implements Database {
+  protected db: Record<string, string> = {};
+  get(id: string): string {
+    return this.db[id];
+  }
+  set(id: string, value: string): void {
+    this.db[id] = value;
+  }
+}
+
+class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db);
+  }
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState);
+  }
+}
+const myDB = new InMemoryDatabase();
+myDB.set("foo", "bar");
+console.log(myDB.get("foo"));
+
+const myPresistentDB = new PersistentMemoryDB();
+myDB.set("foo", "bar");
+console.log(myDB.get("foo"));
+const saved = myPresistentDB.saveToString();
