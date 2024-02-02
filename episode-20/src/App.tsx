d284de8220072f39946f9681020f2a00e96b491f
jack.ooi <jack.ooi@bp.com>
2024-02-02 13:47:10 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch experimentbranch
     Your branch is ahead of 'origin/experimentbranch' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   episode-20/src/App.css
    	modified:   episode-20/src/App.tsx

--- episode-20/src/App.tsx
+++ episode-20/src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import "./App.css";
+
+interface Props {
+  children: React.ReactNode;
+}
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
+
+const Box: React.FC<Props> = ({ children }) => (
+  <div
+    style={{
+      padding: "1rem",
+      fontWeight: "bold",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const List: React.FunctionComponent<{
+  items: string[];
+  onClick?: (item: string) => void;
+}> = ({ items, onClick }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>
+        {item}
+      </li>
+    ))}
+  </ul>
+);
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
+
   return (
     <div>
+      <Heading title="Introduction" />
+      <Box>Hello there</Box>
+      <List items={["one", "two", "three"]} onClick={onListClick} />
     </div>
   );
 }
